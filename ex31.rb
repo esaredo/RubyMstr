8c3eba14ea91269e498097923cc6a757121aa163
Emiliano Saredo <esaredo@amco.mx>
2018-09-14 14:42:15 -0300

    agrego comentario del status del ejercicio 31.

--- ex31.rb
+++ ex31.rb
@@ -1,6 +1,8 @@
 # Ejercicio no comenzado
 # https://learnrubythehardway.org/book/ex31.html
 
+# Verificar porque corno no corre ruby!
+
 puts "You enter a dark room with two doors.  Do you go through door #1 or door #2?"
 
 print "> "
