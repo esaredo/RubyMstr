56d4733dc30bd4dbe1347794a4a5ebbe2ea5238e
Emiliano Saredo <esaredo@amco.mx>
2018-10-18 14:10:07 -0300

    corrección de formato de salida.

--- ex34.rb
+++ ex34.rb
@@ -21,24 +21,24 @@ y = 1
 
 # Imprimo el contenido del array con su referencia numérica
 animals.each do |i|
-  puts "The animal at #{z} is the ##{y} animal, which is #{i}"
+  puts "#{i} is at #{z} and the ##{y}"
   z += 1
   y += 1
 end
 
 # Imprimo el texto sugerido en el ejercicio
 puts """
-The animal at 1 is the second animal and is a #{a}.
-The third animal is the animal at 2 and is #{b}.
-The first animal is the animal at 0 and is #{c}.
-The animal at 3 is the fourth animal and is #{d}.
-The fifth animal is the animal at 4 and is #{e}.
-the animal at 2 is the third animal and is #{f}.
-The sixth animal is the animal at 5 and is #{g}.
-The animal at 4 is the fifth animal and is #{h}.
+1) The animal at 1 is the second animal and is a #{a}.
+2) The third animal is the animal at 2 and is #{b}.
+3) The animal at 3 is the fourth animal and is #{d}.
+4) The first animal is the animal at 0 and is #{c}.
+5) The fifth animal is the animal at 4 and is #{e}.
+6) the animal at 2 is the third animal and is #{f}.
+7) The sixth animal is the animal at 5 and is #{g}.
+8) The animal at 4 is the fifth animal and is #{h}.
 """
 
 # Pude haberlo hecho de otra forma, si.
-# Pero luego de rehacerlo 3 veces decidí que ésta es la mejor forma
+# Pero luego de rehacerlo 5 veces decidí que ésta es la mejor forma
 # de presentación visual del ejercicio
 # (con visual me refiero a como se ve por consola)
